d1d78fa2e790719a25145b1413092d836f422392
SkyBlox152 <74969893+SkyBlox152@users.noreply.github.com>
2021-01-27 18:43:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+var noseX=0;
+var noseY=0;
+
+function preload(){
+    img=loadImage("https://i.postimg.cc/43HTsckB/Dog-Face-snap-removebg-preview.png")
+}
+
+function setup(){
+   canvas=createCanvas(300,300);
+   canvas.center();
+   video=createCapture(VIDEO);
+   video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(img, noseX, noseY, 200, 200);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png')
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    console.log(25)
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x -100;
+        noseY=results[0].pose.nose.y -100;
+        console.log("nose x="+ results[0].pose.nose.x)
+        console.log("nose y=" + results[0].pose.nose.y)
+    }
+}
